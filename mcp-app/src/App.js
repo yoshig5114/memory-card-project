a79f9c97d749049258b09e6248ef4578a8a9cca0
Joshua Gregory <yoshig5114@gmail.com>
2023-06-29 08:34:41 -0400

    added style

--- mcp-app/src/App.js
+++ mcp-app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <h1>MEMORY CARD GAME</h1>
       </header>
-      <body>
+      
         <div className="card-container">
           <div className="card">
             <div className="front">
-              <img src="https://tile.loc.gov/storage-services/service/pnp/cph/3g10000/3g12000/3g12900/3g12901_150px.jpg#h=150&w=121"></img>
+              <img alt="card back with rome" src="https://tile.loc.gov/storage-services/service/pnp/cph/3g10000/3g12000/3g12900/3g12901_150px.jpg#h=150&w=121"></img>
             </div>
           </div>
         </div>
-      </body>
+      
     </div>
   );
 }
