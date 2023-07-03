ee095cf85f07601ae80260693c2769b48a9479e8
Joshua Gregory <yoshig5114@gmail.com>
2023-07-03 19:39:19 -0400

    set up imported variables as useState, functions for tracking seen cards, start game

--- mcp-app/src/App.jsx
+++ mcp-app/src/App.jsx
@@ -5,14 +5,41 @@ import { useState,useEffect } from 'react'
 import Card from './Components/Cards'
 
 function App() {
-  const [card,setCard] = useState([])
+  
   const [reset,setReset] = useState(false)
   const [score,setScore] = useState(0)
   const [highScore,setHighScore] = useState(0)
   const [gameOver,setGameOver] = useState(false)
-  const [shuffle,setShuffle] = useState(false)
+  const [cardBack,setCardBack] = useState(true)
+  const [cardFront,setCardFront] = useState(true)
+  const {card,setCard} = useState(cardBack)
+  const [seenCards,setSeenCards] = useState([])
   
+  useEffect(() => {
+    resetSeenCards()
+  },[])
 
+  function resetSeenCards(){
+    setSeenCards([])
+  }
+
+  function startGame(){
+    let unseenCards =cardFront.filter(card => !seenCards.includes(card))
+    if(unseenCards.length === 0){
+      setGameOver(true)
+      setHighScore(score)
+      setReset(true)
+      resetSeenCards()
+      setCard(unseenCards)
+    }
+    let randomNumber = Math.floor(Math.random() * unseenCards.length);
+    let chosenCard = unseenCards[randomNumber];
+    setCardFront(cardFront.map((card) => (card.id === chosenCard.id ? { ...card, seen: true } : card)));
+    setCard(chosenCard);
+    setSeenCards([...seenCards, chosenCard.id]);
+    setScore(score + 1);
+    setReset(false)  
+  }
 
 
 
@@ -21,9 +48,9 @@ function App() {
       <header className="App-header">
         <h1>MEMORY CARD GAME</h1>
       </header>
-
-      <Card  />
-        
+      <div className="game-container">
+        <Card onClick={startGame} />
+      </div>
       
     </div>
   );
